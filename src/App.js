508831e82050e6da506017affea8f64863e23c01
Noemin Lin <noeminlin@gmail.com>
2022-11-19 21:05:10 +0800

    初步拆分component，複製參考專案html/css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './style/main.scss';
+import StepProgress from './components/StepProgress/StepProgress';
+import Forms from './components/Forms/Form';
+import ProgressControl from './components/ProgressControl/ProgressControl';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <StepProgress />
+      <Forms />
+      <ProgressControl />
+    </>
   );
 }
 
---
+++ src/components/ProgressControl/ProgressControl.js
@@ -0,0 +1,60 @@
+// section
+// button-group
+
+function Progress1() {
+  return (
+    <section className="button-group col col-12" data-phase="address">
+      <button className="next">
+        下一步
+        <svg className="cursor-point">
+          {/* <use xlink:href="#svg-icon-right-arrow"></use> */}
+        </svg>
+      </button>
+    </section>
+  )
+}
+
+function Progress2() {
+  return (
+    <section className="button-group col col-12" data-phase="shipping">
+      <button className="prev">
+        <svg className="cursor-point">
+          {/* <use xlink:href="#svg-icon-left-arrow"></use> */}
+        </svg>
+        上一步
+      </button>
+      <button className="next">
+        下一步
+        <svg className="cursor-point">
+          {/* <use xlink:href="#svg-icon-right-arrow"></use> */}
+        </svg>
+      </button>
+    </section>
+  )
+}
+
+function Progress3() {
+  return (
+    <section className="button-group col col-12" data-phase="credit-card">
+      <button className="prev">
+        <svg className="cursor-point">
+          {/* <use xlink:href="#svg-icon-left-arrow"></use> */}
+        </svg>
+        上一步
+      </button>
+      <button className="next">
+        確認下單
+      </button>
+    </section>
+  )
+}
+
+function ProgressControl() {
+  return (
+    <section className="progress-control-container col col-lg-6 col-sm-12">
+      <Progress1 />
+    </section>
+  )
+}
+
+export default ProgressControl
