d0c3038019beda67f5395399f3f9ab96d5532210
Noemin Lin <noeminlin@gmail.com>
2022-11-20 12:38:13 +0800

    檔名調整

---
+++ src/data/FormsOptionList.js
@@ -0,0 +1,40 @@
+
+const CityList = [
+  { value: "KLU", title: "基隆市" },
+  { value: "TPH", title: "新北市" },
+  { value: "TPE", title: "臺北市" },
+  { value: "TYC", title: "桃園市" },
+  { value: "HSH", title: "新竹縣" },
+  { value: "HSC", title: "新竹市" },
+  { value: "MAC", title: "苗栗市" },
+  { value: "MAL", title: "苗栗縣" },
+  { value: "TXG", title: "臺中市" },
+  { value: "CWH", title: "彰化縣" },
+  { value: "CWS", title: "彰化市" },
+  { value: "NTC", title: "南投市" },
+  { value: "NTO", title: "南投縣" },
+  { value: "YLH", title: "雲林縣" },
+  { value: "CHY", title: "嘉義縣" },
+  { value: "CYI", title: "嘉義市" },
+  { value: "TNN", title: "臺南市" },
+  { value: "KHH", title: "高雄市" },
+  { value: "IUH", title: "屏東縣" },
+  { value: "PTS", title: "屏東市" },
+  { value: "ILN", title: "宜蘭縣" },
+  { value: "ILC", title: "宜蘭市" },
+  { value: "HWA", title: "花蓮縣" },
+  { value: "HWC", title: "花蓮市" },
+  { value: "TTC", title: "臺東市" },
+  { value: "TTT", title: "臺東縣" },
+  { value: "PEH", title: "澎湖縣" },
+  { value: "KMN", title: "金門縣" },
+  { value: "LNN", title: "連江縣" },
+]
+
+const GenderList = [
+  { value: "mr", title: "先生" },
+  { value: "KMN", title: "女士" },
+  { value: "LNN", title: "不明" },
+]
+
+export { CityList, GenderList }
