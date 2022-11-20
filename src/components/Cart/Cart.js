a3a760019b73ca7803b76122e8d275909cd3d190
Noemin Lin <noeminlin@gmail.com>
2022-11-20 17:53:39 +0800

    useState 操作chart 數量點擊 完成數量更變及計數

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,33 +1,34 @@
 import icons from './../../assets/icons/icons.svg'
+import { useState } from 'react'
 
 
-function CountButton({ type }) {
+function CountButton({ type, onClick, id }) {
   return (
     <svg className={"product-action " + type}>
-      <use xlinkHref={icons + "#svg-icon-" + type}></use>
+      <use xlinkHref={icons + "#svg-icon-" + type} onClick={onClick} type={type} id={id}></use>
     </svg>
   )
 }
 
-function ProductControl({ quantity }) {
+function ProductControl({ quantity, onClick, id }) {
   return (
     <div className="product-control-container">
       <div className="product-control">
-        <CountButton type="minus" />
+        <CountButton type="minus" onClick={onClick} id={id} />
         <span className="product-count">{quantity}</span>
-        <CountButton type="plus" />
+        <CountButton type="plus" onClick={onClick} id={id} />
       </div>
     </div>
   )
 }
 
-function Product({ id, name, price, quantity, img }) {
+function Product({ id, name, price, quantity, img, onClick }) {
   return (
     <div className="product-container col col-12" key={id}>
       <img className="img-container" src={img} alt="" />
       <div className="product-info">
         <div className="product-name">{name}</div>
-        <ProductControl quantity={quantity} />
+        <ProductControl quantity={quantity} onClick={onClick} id={id} />
         <div className="price">${price}</div>
       </div>
     </div>
@@ -44,7 +45,7 @@ function CartInfo({ title, value }) {
 }
 
 function Cart() {
-  const productList = [
+  const productListInit = [
     {
       id: '1',
       name: '貓咪罐罐',
@@ -61,6 +62,31 @@ function Cart() {
     },
   ]
 
+  const [productList, setProductList] = useState(productListInit)
+
+  function handleProductQuantity(e) {
+    const id = e.target.id
+    const type = e.target.getAttribute('type')
+    const newProductList = productList.map(prod => {
+      if (prod.id === id) {
+        const newProd = { ...prod }
+        const newQuantity = newProd.quantity + (type === 'minus' ? (-1) : 1)
+        newProd.quantity = newQuantity >= 0 ? newQuantity : 0
+        return newProd
+      } else {
+        return { ...prod }
+      }
+    });
+    setProductList(newProductList)
+  }
+  function countTotal(productList) {
+    let total = 0;
+    productList.forEach(prod => {
+      total += prod.price * prod.quantity
+    });
+    return total
+  }
+
   return (
     <section className="cart-container col col-lg-5 col-sm-12" data-total-price="0">
       <h3 className="cart-title">購物籃</h3>
@@ -68,13 +94,13 @@ function Cart() {
       <section className="product-list col col-12" data-total-price="0">
         {
           productList.map(prod =>
-            <Product {...prod} key={prod.id + '-' + prod.name} />
+            <Product {...prod} key={prod.id + '-' + prod.name} onClick={handleProductQuantity} />
           )
         }
       </section>
 
       <CartInfo title={"運費"} value={"免費"} />
-      <CartInfo title={"小計"} value={"$ " + 0} />
+      <CartInfo title={"小計"} value={"$ " + countTotal(productList)} />
     </section>
   )
 }
